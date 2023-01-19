6f8207c054cc1fc21b47fd72cd100dd579870bf9
Bogdan Grishin <62828748+BOGDAN-GRISHIN@users.noreply.github.com>
2023-01-19 22:57:03 +0200

    Update footer.js

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,5 +1,5 @@
 import styles from './footer.module.css'
 
 export const Footer = () => (<div className={styles.footer}>
-    <a href={"https://verhovna-rada.github.io/summary"}><span>Made with</span> ❤️</a>
+    <a href={"https://bgrishin.me/"}><span>Made with</span> ❤️</a>
 </div>)
